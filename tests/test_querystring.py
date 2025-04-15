00211ac0e0ead6a2c7402a1e01763ff8ae79fa83
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-04-14 22:33:13 -0700

    Query: 112 diseases curated from NEW_MASTER_04-13-2025.csv Elastic query strings now availailable for 112 diseases in latest NEW MASTER sheet.

--- tests/test_querystring.py
+++ tests/test_querystring.py
@@ -4,10 +4,20 @@ from rapid import kql_syntax, filetool
 
 class TestQuerystring(unittest.TestCase):
 
-    def test_generator(self):
-        disease_json = filetool.read_disease_json('disease_names_expanded.json')
+    @unittest.skip('query.json, query.txt')
+    def test_generator(self, filename_json='disease_names_expanded.json'):
+        disease_json = filetool.read_disease_json(filename_json)
+        query_json = dict()
+        query_list = list()
 
         for disease, keyword_list in disease_json.items():
             as_string = kql_syntax.match_phrase_any(keyword_list)
             as_json = kql_syntax.query_string(as_string)
-            print(as_json)
+            query_json[disease] = as_json
+            query_list.append(str(as_json))
+
+        filetool.write_text('\n'.join(query_list),
+                            filetool.resource(f'{filename_json}.query.txt'))
+
+        filetool.write_json(query_json,
+                            filetool.resource(f'{filename_json}.query.json'))
