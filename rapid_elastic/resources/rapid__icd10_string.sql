22d012bf561d1756d76c32ddd28763e474d169b8
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-04-23 14:50:35 -0400

    rapid__icd10_string.sql string representation of unique icd10 codes

---
+++ rapid_elastic/resources/rapid__icd10_string.sql
@@ -0,0 +1,34 @@
+create table rapid__icd10_str as
+WITH distinct_codes AS (
+    SELECT DISTINCT disease_alias, icd10_code, length(icd10_code) as digits
+    FROM rapid__codeset
+    order by disease_alias, digits
+),
+less_specific as
+(
+    select  A.disease_alias, A.icd10_code, A.digits
+    from    distinct_codes A, distinct_codes B
+    where   A.disease_alias = B.disease_alias
+    and     A.icd10_code !=  B.icd10_code
+    and     A.digits < B.digits
+    order by A.disease_alias, A.digits
+),
+more_specific as
+(
+    select * from distinct_codes where disease_alias not in (select disease_alias from less_specific)
+),
+less_and_more as
+(
+    select disease_alias, icd10_code || '*' as icd10_code from less_specific
+    union
+    select disease_alias, icd10_code || '*' as icd10_code from more_specific
+),
+icd10_list as
+(
+    SELECT disease_alias,
+           array_join(array_sort(array_agg(icd10_code)), ', ') AS icd10_str
+    FROM less_and_more
+    GROUP BY disease_alias
+)
+select * from icd10_list
+order by disease_alias;
